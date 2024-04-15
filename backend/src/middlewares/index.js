33c22f12e651df5e6c05f517fcb5aad19ce89209
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-15 15:48:06 +0200

    feat(middleware): update error handler and not found function to accomodate the new NotFoundError error

--- backend/src/middlewares/index.js
+++ backend/src/middlewares/index.js
@@ -1,13 +1,14 @@
 import bcrypt from 'bcrypt';
+import { NotFoundError } from '../utils/errores/index.js';
 
-export function noEncontrado(_, res) {
-  res.status(404).json({ mensaje: 'Error 404: No encontrado' });
+export function noEncontrado() {
+  throw new NotFoundError('Recurso no encontrado');
 }
 
 export function manejadorErrores(error, req, res, next) {
-  const estado = error.status || 500;
+  const estado = error.status || error.estado || 500;
 
-  const mensaje = error.mensaje || error.message || 'Algo salió mal';
+  const mensaje = error.message || error.mensaje || 'Algo salió mal';
 
   res.status(estado).json({ error: mensaje });
 }
