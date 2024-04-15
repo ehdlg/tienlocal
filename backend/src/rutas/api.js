6be002dc0543788be68d60ac8b175dd3276881c3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-15 12:04:01 +0200

    feat(routes): implement products route

--- backend/src/rutas/api.js
+++ backend/src/rutas/api.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import rutaUsuarios from './usuario.ruta.js';
+import rutaProductos from './producto.ruta.js';
 
 const router = Router();
 
 router.get('/', (req, res, next) => {
   res.json({ mensaje: 'Bienvenido a la API de Tienlocal' });
 });
+
+router.use(rutaProductos);
 
 router.use(rutaUsuarios);
 
