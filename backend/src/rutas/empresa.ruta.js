0cbc69e8493c54ed3b755eb330fb742199ac3b74
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-16 12:57:03 +0200

    feat(validation): implement update and create rules for empresa

--- backend/src/rutas/empresa.ruta.js
+++ backend/src/rutas/empresa.ruta.js
@@ -2,17 +2,30 @@ import { Router } from 'express';
 import { crearEmpresaReglas, actualizarEmpresaReglas } from '../middlewares/validacion/empresa.js';
 import EmpresaControlador from '../controladores/Empresa.controlador.js';
 import { validacion } from '../middlewares/validacion/index.js';
+import { generarHashedPassword } from '../middlewares/index.js';
 
 const router = Router();
 
 router.get('/empresas/:id', EmpresaControlador.obtenerUno);
 
-router.patch('/empresas/:id', actualizarEmpresaReglas, validacion, EmpresaControlador.actualizar);
+router.patch(
+  '/empresas/:id',
+  actualizarEmpresaReglas,
+  validacion,
+  generarHashedPassword,
+  EmpresaControlador.actualizar
+);
 
 router.delete('/empresas/:id', EmpresaControlador.borrar);
 
 router.get('/empresas', EmpresaControlador.obtenerTodos);
 
-router.post('/empresas', crearEmpresaReglas, validacion, EmpresaControlador.crear);
+router.post(
+  '/empresas',
+  crearEmpresaReglas,
+  validacion,
+  generarHashedPassword,
+  EmpresaControlador.crear
+);
 
 export default router;
