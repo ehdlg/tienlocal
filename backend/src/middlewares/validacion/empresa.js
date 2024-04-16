e12d5ecbbb7d8ecc87c1335414065e4d1be07949
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-16 11:08:46 +0200

    feat(validation): add create and update Empresa chain validation

---
+++ backend/src/middlewares/validacion/empresa.js
@@ -0,0 +1,79 @@
+import { body, param } from 'express-validator';
+import { comprobarEmail } from './utils.js';
+import { regexContrasena } from './constants.js';
+
+export const crearEmpresaReglas = (() => {
+  return [
+    body('nombre')
+      .exists()
+      .withMessage('Introduce el nombre de la empresa')
+      .bail()
+      .notEmpty()
+      .withMessage('El nombre de la empresa no puede estar vacío')
+      .isLength({ min: 1, max: 100 })
+      .withMessage('El nombre de la empresa no puede ocupar más de 100 caracteres'),
+
+    body('email').exists().isEmail().custom(comprobarEmail),
+
+    body('contrasena')
+      .exists()
+      .withMessage('Introduce una contraseña')
+      .bail()
+      .matches(regexContrasena)
+      .withMessage(
+        `La contraseña debe contener una minúscula, 
+        una mayúscula, un número y mínimo 8 caracteres`
+      ),
+
+    body('descripcion')
+      .exists()
+      .withMessage('Introduce una descripción para la empresa')
+      .bail()
+      .notEmpty()
+      .withMessage('La descripción no puede estar vacía'),
+
+    body('ubicacion')
+      .exists()
+      .withMessage('Introduce una ubicación para la empresa')
+      .bail()
+      .notEmpty()
+      .withMessage('La ubación no puede estar vacía'),
+  ];
+})();
+
+export const actualizarEmpresaReglas = (() => {
+  return [
+    param('id')
+      .exists()
+      .withMessage('Introduce un ID de empresa')
+      .bail()
+      .isInt()
+      .withMessage('El ID de empresa introducido no es válido'),
+
+    body('nombre')
+      .optional()
+      .notEmpty()
+      .withMessage('El nombre de la empresa no puede estar vacío')
+      .bail()
+      .isLength({ min: 1, max: 100 })
+      .withMessage('El nombre de la empresa no puede ocupar más de 100 caracteres'),
+
+    body('email').exists().isEmail().custom(comprobarEmail),
+
+    body('contrasena')
+      .optional()
+      .matches(regexContrasena)
+      .withMessage(
+        `La contraseña debe contener una minúscula, 
+        una mayúscula, un número y mínimo 8 caracteres`
+      ),
+
+    body('descripcion')
+      .exists()
+      .withMessage('Introduce una descripción para la empresa')
+      .notEmpty()
+      .withMessage('La descripción no puede estar vacía'),
+
+    body('ubicacion').exists().withMessage('Introduce una ubicación para la empresa'),
+  ];
+})();
