e05f852f267f36b37d259ea0041c0b64e3c35a7e
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-24 09:05:59 +0200

    feat(styles): add NoEncontrado component styles

--- frontend/src/componentes/NoEncontrado.jsx
+++ frontend/src/componentes/NoEncontrado.jsx
@@ -1,9 +1,21 @@
+import Header from './Header';
+import Footer from './Footer';
+import { Link } from 'react-router-dom';
+import estilos from '../estilos/NoEncontrado.module.css';
+
 function NoEncontrado() {
   return (
-    <main>
-      <h1>Página no encontrada</h1>
-      <p>Lo sentimos, la página que estás buscando no existe</p>
-    </main>
+    <>
+      <Header />
+      <main className={estilos.wrapper}>
+        <h1>Página no encontrada</h1>
+        <p className={estilos.mensaje}>Lo sentimos, la página que estás buscando no existe</p>
+        <Link to={'/'} className={estilos.redireccion}>
+          Ir a la página principal
+        </Link>
+      </main>
+      <Footer />
+    </>
   );
 }
 
