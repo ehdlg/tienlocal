dafcad0c655be2bca1d433624d1f65e328d73687
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-25 15:30:35 +0200

    feat(Header): add tienlocal logo and styles to the header component

--- frontend/src/componentes/Header.jsx
+++ frontend/src/componentes/Header.jsx
@@ -6,8 +6,9 @@ function Header() {
   return (
     <header className={estilos.wrapper}>
       <div className={estilos.content}>
-        <Link className='link' to={'/'}>
-          <span className={estilos.titulo}>Tienlocal</span>
+        <Link className={`link ${estilos.logoTitulo}`} to={'/'}>
+          <span className={estilos.titulo}>TienLocal</span>
+          <img src='logo.png' alt='Logo de Tienlocal' className={estilos.logo} />
         </Link>
         <Navbar />
       </div>
