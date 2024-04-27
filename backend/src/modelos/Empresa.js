2dd395283421479ca64440071925b6e91eee0215
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-27 12:23:56 +0200

    feat(Empresa): add model function to get Empresa without credentials, update controller and route reflect this new function

--- backend/src/modelos/Empresa.js
+++ backend/src/modelos/Empresa.js
@@ -2,4 +2,16 @@ import Login from './Login.js';
 
 export default class Empresa extends Login {
   static tabla = 'empresas';
+
+  static async ObtenerTodosSinCredenciales() {
+    const consulta = 'SELECT id, nombre, descripcion, ubicacion FROM empresas;';
+
+    try {
+      const empresas = await this.db.query(consulta);
+
+      return empresas;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
