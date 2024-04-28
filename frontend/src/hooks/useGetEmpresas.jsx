d124b67bf8a577fc1a78aa21f47c19d07a79a2b2
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-28 10:25:49 +0200

    feat(hook): add useGetEmpresa hook

---
+++ frontend/src/hooks/useGetEmpresas.jsx
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from 'react';
+import { API_URL } from '../constantes';
+function useGetEmpresas() {
+  const [empresas, setEmpresas] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const obtenerEmpresas = useCallback(async () => {
+    const URL = `${API_URL}/empresas`;
+
+    setError(null);
+    setLoading(false);
+
+    try {
+      const respuesta = await fetch(URL);
+
+      console.log(respuesta);
+      if (respuesta.status !== 200)
+        return setError('No se ha podido establecer conexión con la base de datos');
+
+      const datos = await respuesta.json();
+
+      setEmpresas(datos);
+    } catch (error) {
+      setError('No se ha podido establecer conexión con la base de datos');
+    }
+  }, []);
+
+  useEffect(() => {
+    obtenerEmpresas();
+  }, []);
+
+  return { empresas, error, loading };
+}
+
+export default useGetEmpresas;
