c2579d364c0c88e2ddfc4ac0b81624c1e7faa6b6
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-05-03 08:49:16 +0200

    feat(components): add Formulario component and styles

---
+++ frontend/src/componentes/Formulario.jsx
@@ -0,0 +1,31 @@
+import estilos from '../estilos/Formulario.module.css';
+
+function Formulario({ inputs, manejarFormulario, textoSubmit, children }) {
+  return (
+    <form className={estilos.formulario} onSubmit={manejarFormulario}>
+      {children}
+      {inputs.map((input) => {
+        const inputId = `registro-${input.name}`;
+        return (
+          <label htmlFor={inputId} key={inputId} className={estilos.label}>
+            {input.label}
+            <input
+              className={input.type == 'text' || input.type == 'password' ? estilos.inputText : ''}
+              type={input.type}
+              name={input.name}
+              id={inputId}
+              key={input.name}
+              required={input.required}
+            />
+          </label>
+        );
+      })}
+
+      <button className={estilos.boton} type='submit'>
+        {textoSubmit}
+      </button>
+    </form>
+  );
+}
+
+export default Formulario;
