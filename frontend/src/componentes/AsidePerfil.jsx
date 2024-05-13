1d640575c63a1cf0430ca055429735d0e4c5ceff
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-05-13 17:10:15 +0200

    feat(component): add svg icon to aside link profile view

--- frontend/src/componentes/AsidePerfil.jsx
+++ frontend/src/componentes/AsidePerfil.jsx
@@ -1,8 +1,9 @@
 import { Link, useLocation } from 'react-router-dom';
-import { UserIcon, KeyIcon } from '@heroicons/react/24/outline';
-import estilos from '../estilos/Perfil.module.css';
+import { UserIcon, KeyIcon, BuildingStorefrontIcon, ShoppingBagIcon } from '@heroicons/react/24/outline';
 import { useContext } from 'react';
 import { Contexto } from '../context';
+import estilos from '../estilos/Perfil.module.css';
+
 function AsidePerfil() {
   const { esEmpresa } = useContext(Contexto);
   const { pathname } = useLocation();
@@ -22,10 +23,12 @@ function AsidePerfil() {
       </Link>
       {esEmpresa ? (
         <Link to={'productos'} className={estilos.link} data-seleccionado={productosSeleccionado}>
+          <BuildingStorefrontIcon />
           Mis productos
         </Link>
       ) : (
         <Link to={'compras'} className={estilos.link} data-seleccionado={'sojdf'}>
+          <ShoppingBagIcon />
           Mis compras
         </Link>
       )}
