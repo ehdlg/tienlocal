17018bf7bb44ee9a6bdf7c8f46187b944f015a3c
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-05-13 17:00:23 +0200

    feat(components): add AsidePerfil component

---
+++ frontend/src/componentes/AsidePerfil.jsx
@@ -0,0 +1,36 @@
+import { Link, useLocation } from 'react-router-dom';
+import { UserIcon, KeyIcon } from '@heroicons/react/24/outline';
+import estilos from '../estilos/Perfil.module.css';
+import { useContext } from 'react';
+import { Contexto } from '../context';
+function AsidePerfil() {
+  const { esEmpresa } = useContext(Contexto);
+  const { pathname } = useLocation();
+
+  const editarContrasena = pathname.includes('contrasena');
+  const productosSeleccionado = pathname.includes('productos');
+
+  return (
+    <aside className={estilos.aside}>
+      <Link to={''} className={estilos.link} data-seleccionado={!editarContrasena}>
+        <UserIcon />
+        Perfil
+      </Link>
+      <Link to={'contrasena'} className={estilos.link} data-seleccionado={editarContrasena}>
+        <KeyIcon />
+        Contraseña
+      </Link>
+      {esEmpresa ? (
+        <Link to={'productos'} className={estilos.link} data-seleccionado={productosSeleccionado}>
+          Productos
+        </Link>
+      ) : (
+        <Link to={'compras'} className={estilos.link} data-seleccionado={'sojdf'}>
+          Mis compras
+        </Link>
+      )}
+    </aside>
+  );
+}
+
+export default AsidePerfil;
