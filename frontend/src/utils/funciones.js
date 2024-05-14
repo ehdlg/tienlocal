f03552911dc432749ab4b4dc69a05d779ac86c06
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-05-14 15:29:57 +0200

    feat(utils): add validarNuevoProducto function

--- frontend/src/utils/funciones.js
+++ frontend/src/utils/funciones.js
@@ -1,6 +1,5 @@
 export function comprobarFiltros(filtros) {
-  const filtrosValidos =
-    null != filtros && typeof filtros === 'object' && Object.keys(filtros).length > 0;
+  const filtrosValidos = null != filtros && typeof filtros === 'object' && Object.keys(filtros).length > 0;
 
   return filtrosValidos;
 }
@@ -13,4 +12,37 @@ export function crearArrayPaginas(paginas) {
   return Array(paginas)
     .fill()
     .map((_, index) => index + 1);
+}
+
+export function validarNuevoProducto(datosFomulario) {
+  const errores = [];
+  let nuevoProducto = { ...datosFomulario };
+
+  if (null == nuevoProducto.nombre || nuevoProducto.nombre == '') {
+    errores.push('Debes introducir un nombre para el producto');
+  }
+
+  if (nuevoProducto.precio <= 0) {
+    errores.push('El precio debe ser un número mayor a 0');
+  }
+
+  if (nuevoProducto.stock < 0) {
+    errores.push('El stock no puede ser un número menor a 0');
+  }
+
+  if (null == nuevoProducto.descripcion || nuevoProducto.descripcion == '') {
+    errores.push('Debes indicar una descripción para el producto');
+  }
+
+  const idCategoria = Number(nuevoProducto['id_categoria']);
+
+  if (isNaN(idCategoria) || idCategoria <= 0) {
+    errores.push('Debes indiciar una categoría');
+  }
+
+  if (datosFomulario.imagen == '') {
+    delete nuevoProducto.imagen;
+  }
+
+  return { errores, nuevoProducto };
 }
