d33c1869a4dd8a91487ced1cb38e6c631e6ef915
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-05-14 15:30:57 +0200

    feat(Input): add min and max values to Input component

--- frontend/src/componentes/Input.jsx
+++ frontend/src/componentes/Input.jsx
@@ -1,19 +1,23 @@
 import estilosFormulario from '../estilos/Formulario.module.css';
 
-function Input({ textoLabel, id, type, name, defaultValue = '', required = false, min, max }) {
+function Input({ textoLabel, id, type, name, defaultValue = '', required = false, min = undefined, max = undefined }) {
+  const estilosInput =
+    type == 'text' || type == 'password' || type == 'email'
+      ? estilosFormulario.inputText
+      : estilosFormulario.inputNumber;
   return (
     <label htmlFor={id} key={id} className={estilosFormulario.label}>
       {textoLabel}
       <input
-        className={type == 'text' || type == 'password' || type == 'email' ? estilosFormulario.inputText : ''}
+        className={estilosInput}
         type={type}
         name={name}
         defaultValue={defaultValue}
         id={id}
         key={name}
         required={required}
-        min={type == 'number' && min ? min : ''}
-        max={type == 'number' && max ? max : ''}
+        min={min}
+        max={max}
       />
     </label>
   );
