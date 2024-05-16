b0f4b5465dad9eb30e94a88cb098228d4fdf23e2
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-05-16 18:54:59 +0200

    feat(Footer): update Footer

--- frontend/src/componentes/Footer.jsx
+++ frontend/src/componentes/Footer.jsx
@@ -1,5 +1,34 @@
+import { Link } from 'react-router-dom';
+import estilos from '../estilos/Footer.module.css'; // Importa los estilos CSS Modules
+
 function Footer() {
-  return <footer>This is the footer</footer>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className={estilos.wrapper}>
+      <div className={estilos.logo}>
+        <p>
+          Descubre y apoya productos locales de alta calidad a través de nuestra plataforma comunitaria dedicada al
+          comercio justo y sostenible.
+        </p>
+      </div>
+
+      <div className={estilos.links}>
+        <ul>
+          <li>
+            <Link to={'sobre-nosotros'}>Sobre nosotros</Link>
+          </li>
+          <li>
+            <Link to={'ayuda'}>Ayuda</Link>
+          </li>
+          <li>Redes sociales</li>
+        </ul>
+      </div>
+
+      <div className={estilos.copyright}>
+        <p>{year} TienLocal. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
