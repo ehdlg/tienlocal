4324679c0ab5a646c087788288548780a7c72108
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-05-16 18:57:43 +0200

    feat(Root): render Carrito when user is logged in

--- frontend/src/componentes/Root.jsx
+++ frontend/src/componentes/Root.jsx
@@ -1,13 +1,19 @@
 import Header from './Header';
 import Main from './Main';
+import Carrito from './Carrito';
 import Footer from './Footer';
+import { useContext } from 'react';
+import { Contexto } from '../context';
 
 function Root() {
+  const { esUsuario } = useContext(Contexto);
+
   return (
     <>
       <Header />
       <Main />
       <Footer />
+      {esUsuario && <Carrito />}
     </>
   );
 }
